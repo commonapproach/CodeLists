600c2cbdaa103c33c7c3188945bbac1ed7691631
Derek Laventure <sensespidey@gmail.com>
2025-06-23 14:04:34 -0300

    Add index.php to mirror live DocumentRoot, with adjusted links to match git repository.

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+<meta charset="utf-8" />
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FMBZVRDNYK"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-FMBZVRDNYK');
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Poppins');
+#headline {
+    font-family: 'Poppins';
+    font-weight: bold;
+    font-size: 48px;
+    text-align: center;
+//    color: #a7d86d;
+    color: #6ca7e0;
+    display: inline-block;
+    margin: auto;
+    width: 100%;
+}
+button {
+ font-family: 'Poppins';
+ font-weight: bold;
+ font-size: 24px;
+ //background-color: #a7d86d;
+ background-color: #6ca7e0;
+ color: #EEEEEE;
+ padding: 10px;
+ float: right;
+ text-align: center;
+ text-decoration: none;
+ margin: 4px 1px;
+ border-radius: 85%;
+ border: 0px;
+}
+#topbanner {
+ height: 100%;
+ width: 100%;
+ display: inline-block;
+}
+#mytable {
+ width: 95%;
+ margin: auto;
+ padding-top: 30px;
+}
+
+.atable  {
+ width: 95%;
+ margin: auto;
+ padding-top: 30px;
+}
+
+th {
+ background-color: #6ca7e0;
+ line-height: 24px;
+ font-weight: bold;
+ border-right: solid 2px #6ca7e0;
+ border-left: solid 2px #6ca7e0;
+}
+table { border-collapse: collapse;
+border-radius: 6px;
+-moz-border-radius: 6px;
+ border-spacing: 2px;
+}
+tr { border: none; }
+td {
+  border-right: solid 2px #6ca7e0;
+  border-left: solid 2px #6ca7e0;
+  border-top: solid 2px #6ca7e0;
+  border-bottom: solid 2px #6ca7e0;
+
+}
+</style>
+<title>Common Approach Code List Server</title>
+</head>
+<body>
+<?php
+?>
+<div id="topbanner">
+<button id="aboutbutton" class="button">About</button><button id="buttonfaq" class="button">FAQ</button>
+</div>
+<div id="headline">Common Approach Code List Server</div>
+<table id="mytable">
+ <tr style="!important; color:#000000;empty-cells:show;">
+  <th style="width: 5%; text-align: center;">Name</th>
+  <th style="width:325px;">Description</th>
+  <th style="width:10%; text-align: center;">Last Update</th>
+  <th style="width:10px; text-align: center;">Issues</th>
+ </tr>
+ <tr>
+  <td><a href="IrisMetrics/IrisMetric53-en.html">IrisMetrics53</a> (<a href="IrisMetrics/IrisMetric53.owl">rdf/xml</a> / <a href="IrisMetrics/IrisMetric53.ttl">turtle</a> / <a href="IrisMetrics/IrisMetric53.jsonld">json-ld</a> / <a href="IrisMetrics/IrisMetric53.nt">triples</a> / <a href="IrisMetrics/IrisMetric53.csv">csv</a>)</td>
+  <td>IRIS+ is the generally accepted system for measuring, managing, and optimizing impact. This Cost List is an implementation of the GIIN IRIS+ System metrics in an RDF data model.
+  </td>
+  <td style="text-align: center;">Oct 1, 2024</td>
+  <td style="text-align: center;">0</td>
+ </tr>
+ <tr>
+  <td><a href="ICNPOsector/ICNPOsector-en.html">ICNPOsector</a> (<a href="ICNPOsector/ICNPOsector.owl">rdf/xml</a> / <a href="ICNPOsector/ICNPOsector.ttl">turtle</a> / <a href="ICNPOsector/ICNPOsector.jsonld">json-ld</a> / <a href="ICNPOsector/ICNPOsector.nt">triples</a> / <a href="ICNPOsector/ICNPOsector.csv">csv</a>)</td>
+  <td>This list represent a possible categorization of sectors as defined by the International Classification of Nonprofit Organizations
+    and republished by Common Approach to Impact Measurement as an RDF codelist.</td>
+  <td style="text-align: center;">Oct 1, 2024</td>
+  <td style="text-align: center;">0</td>
+ </tr>
+ <tr>
+  <td><a href="StatsCanSector/StatsCanSector-en.html">StatsCanSector</a> (<a href="StatsCanSector/StatsCanSector.owl">rdf/xml</a> / <a href="StatsCanSector/StatsCanSector.ttl">turtle</a> / <a href="StatsCanSector/StatsCanSector.jsonld">json-ld</a> / <a href="StatsCanSector/StatsCanSector.nt">triples</a> / <a href="StatsCanSector/StatsCanSector.csv">csv</a>)</td>
+  <td>This list represent a possible categorization of sectors as defined by Statistics Canada and republished by Common Approach to Impact Measurement as an RDF codelist.</td>
+  <td style="text-align: center;">Oct 3, 2024</td>
+  <td style="text-align: center;">0</td>
+ </tr>
+
+ <tr>
+  <td><a href="PopulationServed/PopulationServed-en.html">PopulationServed</a> (<a href="PopulationServed/PopulationServed.owl">rdf/xml</a> / <a href="PopulationServed/PopulationServed.ttl">turtle</a> / <a href="PopulationServed/PopulationServed.jsonld">json-ld</a> / <a href="PopulationServed/PopulationServed.nt">triples</a> / <a href="PopulationServed/PopulationServed.csv">csv</a>)</td>
+  <td>A codelist vocabulary of Populations Served.</td>
+  <td style="text-align: center;">Oct 8, 2024</td>
+  <td style="text-align: center;">0</td>
+ </tr>
+
+ <tr>
+  <td><a href="ProvinceTerritory/ProvinceTerritory-en.html">ProvinceTerritory</a> (<a href="ProvinceTerritory/ProvinceTerritory.owl">rdf/xml</a> / <a href="ProvinceTerritory/ProvinceTerritory.ttl">turtle</a> / <a href="ProvinceTerritory/ProvinceTerritory.jsonld">json-ld</a> / <a href="ProvinceTerritory/ProvinceTerritory.nt">triples</a> / <a href="ProvinceTerritory/ProvinceTerritory.csv">csv</a>)</td>
+  <td>A codelist vocabulary of Province and Territory Codelists according to Statistics Canada definitions.</td>
+  <td style="text-align: center;">Oct 8, 2024</td>
+  <td style="text-align: center;">0</td>
+ </tr>
+
+ <tr>
+  <td><a href="EquityDeservingGroupsESDC/EquityDeservingGroupsESDC-en.html">EquityDeservingGroupsESDC</a> (<a href="EquityDeservingGroupsESDC/EquityDeservingGroupsESDC.owl">rdf/xml</a> / <a href="EquityDeservingGroupsESDC/EquityDeservingGroupsESDC.ttl">turtle</a> / <a href="EquityDeservingGroupsESDC/EquityDeservingGroupsESDC.jsonld">json-ld</a> / <a href="EquityDeservingGroupsESDC/EquityDeservingGroupsESDC.nt">triples</a> / <a href="EquityDeservingGroupsESDC/EquityDeservingGroupsESDC.csv">csv</a>)</td>
+  <td>A codelist vocabulary of Equity Deserving Groups as defined by Ministry of Employment and Social Development of Canada.</td>
+  <td style="text-align: center;">Oct 3, 2024</td>
+  <td style="text-align: center;">0</td>
+ </tr>
+
+ <tr>
+  <td><a href="SDGImpacts/SDGImpacts-en.html">SDGImpacts</a> (<a href="SDGImpacts/SDGImpacts.owl">rdf/xml</a> / <a href="SDGImpacts/SDGImpacts.ttl">turtle</a> / <a href="SDGImpacts/SDGImpacts.jsonld">json-ld</a> / <a href="SDGImpacts/SDGImpacts.nt">triples</a> / <a href="SDGImpacts/SDGImpacts.csv">csv</a>)</td>
+  <td>A codelist vocabulary of Sustainable Development Goals (SDGs) Taxonomy of Impact Themes</td>
+  <td style="text-align: center;">Oct 7, 2024</td>
+  <td style="text-align: center;">0</td>
+ </tr>
+
+ <tr>
+  <td><a href="OrgTypeGOC/OrgTypeGOC-en.html">OrgTypeGOC</a> (<a href="OrgTypeGOC/OrgTypeGOC.owl">rdf/xml</a> / <a href="OrgTypeGOC/OrgTypeGOC.ttl">turtle</a> / <a href="OrgTypeGOC/OrgTypeGOC.jsonld">json-ld</a> / <a href="OrgTypeGOC/OrgTypeGOC.nt">triples</a> / <a href="OrgTypeGOC/OrgTypeGOC.csv">csv</a> )</td>
+  <td>A codelist vocabulary of organization types informed by Government of Canada definitions.</td>
+  <td style="text-align: center;">Oct 7, 2024</td>
+  <td style="text-align: center;">0</td>
+ </tr>
+
+ <tr>
+  <td><a href="Locality/LocalityStatsCan-en.html">LocalityStatsCan</a> (<a href="Locality/LocalityStatsCan.owl">rdf/xml</a> / <a href="Locality/LocalityStatsCan.ttl">turtle</a> / <a href="Locality/LocalityStatsCan.jsonld">json-ld</a> / <a href="Locality/LocalityStatsCan.nt">triples</a> / <a href="Locality/LocalityStatsCan.csv">csv</a> )</td>
+  <td>A codelist vocabulary of Locality Codelists according to Statistics Canada definitions.</td>
+  <td style="text-align: center;">Oct 7, 2024</td>
+  <td style="text-align: center;">0</td>
+ </tr>
+
+
+</table>
+<div style="margin-bottom:10px;"></div>
+<table id="mytable1" class="atable">
+ <tr style="!important; color:#000000;empty-cells:show;">
+  <th style="text-align: center;">Documentation</th>
+ </tr>
+ <tr><td> </td></tr>
+ </table>
+</body>
+</html>
+
+
