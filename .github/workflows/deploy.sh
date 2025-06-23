7a4860a0cfd794612364611dbd8824279e2d6521
Derek Laventure <sensespidey@gmail.com>
2025-06-23 14:12:19 -0300

    Add .github Actions config to publish Codelists to webserver.

---
+++ .github/workflows/deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# This script runs in the Github CI environment. It sets up SSH access to the server,
+# and rsync's the `public` folder into place.
+
+# Create the .ssh directory on the runner. Used to store the ssh key for authentication
+export SSHDIR="$HOME/.ssh"
+mkdir -p "$SSHDIR"
+
+# Copy the ssh key from the secrets store into the .ssh directory and assign the correct permissions
+echo "$SSH_PRIVATE_KEY" > "$SSHDIR/key"
+chmod 600 "$SSHDIR/key"
+
+# Setup a variable to pass to ssh for the connection to the server. This
+# includes the DOCROOT path to the public-facing web directory.
+export SERVER_DEPLOY_STRING="$SSH_USERNAME@$SSH_SERVER:$SSH_DOCROOT"
+
+# Change to the public directory and rsync everything to the documents location for the web server on my server 
+cd "$GITHUB_WORKSPACE/public" || exit 1
+/usr/bin/rsync -hlvcr --progress --delete --no-o --no-g -e "ssh -i $SSHDIR/key -o StrictHostKeyChecking=no" . "$SERVER_DEPLOY_STRING"
